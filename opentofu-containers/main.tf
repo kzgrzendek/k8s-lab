4e5f5d288026567fd52c3ab997623d0986f029f4
Kévin ZGRZENDEK <kevin.zgrzendek@aphp.fr>
2025-09-10 13:53:17 +0200

    Refactored repo structured and init docker opentofu provider

---
+++ opentofu-containers/main.tf
@@ -0,0 +1,34 @@
+# Set the required provider and versions
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+  }
+}
+
+# Configure the docker provider
+provider "docker" {
+}
+
+# Create a docker image resource
+# -> docker pull nginx:latest
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
+resource "docker_container" "nginx" {
+  name    = "nginx"
+  image   = docker_image.nginx.image_id
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
